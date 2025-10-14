5c775323f245ca068f7224500a564632400cd7ec
BharatSharma34 <bharatsharma@ducktaleit.com>
2025-10-14 16:07:27 +0530

    Create tool.py

---
+++ tool.py
@@ -0,0 +1,16 @@
+# Add your tool logic here
+def example_function(input_data):
+    """
+    Replace this with your actual tool function.
+    This function will be automatically called when your tool is invoked.
+    
+    Parameters:
+    input_data: The data passed to your tool
+    
+    Returns:
+    Any JSON-serializable data
+    """
+    return {"message": "Hello from TestTool1oct14!", "input": input_data}
+
+# You can add multiple functions here
+# The last defined function will be used as the main tool function
